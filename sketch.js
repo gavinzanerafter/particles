aa368870f5d03d060231730361ae18d29e7b6028
gavinzanerafter <gavinzanerafter@gmail.com>
2025-10-17 15:05:21 +0000

    Fix error

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ function setup() {
  // put setup code here
  createCanvas(window.width,window.height)
  for (var i = 0; i < particles.length; i++) {
-  particles[i] = new Particle((i % 10) * width / 10 + width / 20, Math.floor(i/10) * height / 10 + height / 20)
+  particles[i] = new Particle((i % 10) * width / 10 + width / 20, Math.floor(i/10) * height / 10 + height / 20, "blue")
  }
 }
 
