f6a418ca0ba915969db22fc90fc87da0c203e89e
gavinzanerafter <gavinzanerafter@gmail.com>
2025-10-17 15:18:07 +0000

    Fix window size

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ let particles = new Array(100)
 
 function setup() {
  // put setup code here
- createCanvas(window.width,window.height)
+ createCanvas(windowWidth,windowHeight)
  for (var i = 0; i < particles.length; i++) {
   particles[i] = new Particle((i % 10) * width / 10 + width / 20, Math.floor(i/10) * height / 10 + height / 20, "blue")
  }
